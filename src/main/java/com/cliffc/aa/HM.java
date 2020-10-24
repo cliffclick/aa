306f6604c340551ef1f2a80afb0d9727eaf00962
Cliff Click <cliffc@acm.org>
2020-10-24 11:15:10 -0700

    remove extra HMFun class

--- src/main/java/com/cliffc/aa/HM.java
+++ src/main/java/com/cliffc/aa/HM.java
@@ -8,12 +8,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
-// Hindley-Milner typing.
+// Hindley-Milner typing.  Complete stand-alone, for research.
+// MEETs base types, instead of declaring type error.
+// Does standard lexical scoping, which is not needed for SSA form.
 public class HM {
-  // Forwards-walk all Nodes
-  public static void HM(Node scope) {
-    //scope.walk_hm(new VBitSet(),new HashSet<>());
-  }
   public static HMType HM(Syntax prog) {
 
     HashMap<String,HMType> env = new HashMap<>();
@@ -24,14 +22,14 @@ public class HM {
     // Primitives
     HMVar var1 = new HMVar();
     HMVar var2 = new HMVar();
-    env.put("pair",new HMFun(var1, new HMFun(var2, new Oper("pair",var1,var2))));
+    env.put("pair",Oper.fun(var1, Oper.fun(var2, new Oper("pair",var1,var2))));
 
     HMVar var3 = new HMVar();
-    env.put("if/else",new HMFun(bool,new HMFun(var3,new HMFun(var3,var3))));
+    env.put("if/else",Oper.fun(bool,Oper.fun(var3,Oper.fun(var3,var3))));
 
-    env.put("dec",new HMFun(int64,int64));
-    env.put("*",new HMFun(int64,new HMFun(int64,int64)));
-    env.put("==0",new HMFun(int64,bool));
+    env.put("dec",Oper.fun(int64,int64));
+    env.put("*",Oper.fun(int64,Oper.fun(int64,int64)));
+    env.put("==0",Oper.fun(int64,bool));
 
     return prog.hm(env, new HashSet<>());
   }
@@ -74,7 +72,7 @@ public class HM {
       HMType trez = _body.hm(env,nongen);
       nongen.remove(tnew);
       env.remove(_arg0);
-      return new HMFun(tnew,trez);
+      return Oper.fun(tnew,trez);
     }
   }
   public static class Let extends Syntax {
@@ -103,7 +101,7 @@ public class HM {
       HMType tfun = _fun.hm(env,nongen);
       HMType targ = _arg.hm(env,nongen);
       HMType trez = new HMVar();
-      HMType nfun = new HMFun(targ,trez);
+      HMType nfun = Oper.fun(targ,trez);
       nfun.union(tfun);
       return trez;
     }
@@ -134,8 +132,6 @@ public class HM {
         HMType[] args = new HMType[op._args.length];
         for( int i=0; i<args.length; i++ )
           args[i] = op._args[i]._fresh(nongen,vars);
-
-        if( op instanceof HMFun  ) return new HMFun (args[0],args[1]);
         return new Oper(op._name,args);
       }
     }
@@ -208,8 +204,14 @@ public class HM {
     final String _name;
     final HMType[] _args;
     Oper(String name, HMType... args) { _name=name; _args=args; }
-    @Override public String toString() { return _name+" "+Arrays.toString(_args); }
+    static Oper fun(HMType... args) { return new Oper("->",args); }
+    @Override public String toString() {
+      if( _name.equals("->") ) return "{ "+_args[0]+" -> "+_args[1]+" }";
+      return _name+" "+Arrays.toString(_args);
+    }
     @Override public String _str() {
+      if( _name.equals("->") ) 
+            return "{ "+_args[0].str()+" -> "+_args[1].str()+" }";
       SB sb = new SB().p(_name).p('(');
       for( HMType t : _args )
         sb.p(t.str()).p(',');
@@ -228,12 +230,4 @@ public class HM {
       return this;
     }
   }
-  static class HMFun extends Oper {
-    HMFun(HMType arg, HMType ret) { super("->",arg,ret); }
-    @Override public String toString() { return "{ "+_args[0]+" -> "+_args[1]+" }"; }
-    @Override public String _str() {
-      return "{ "+_args[0].str()+" -> "+_args[1].str()+" }";
-    }
-  }
-
 }
