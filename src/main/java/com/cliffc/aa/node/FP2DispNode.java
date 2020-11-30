31bf5eede63a55d69f846498a7e332df71075721
Cliff Click <cliffc@acm.org>
2020-11-30 14:26:06 -0800

    FP2Disp fixes
    Cleans out from Unresolved with same displays.
    Live_use reports mem-alive from Threts (which take shortcuts anyways) but otherwise its just normal liveness from a standard pointer.

--- src/main/java/com/cliffc/aa/node/FP2DispNode.java
+++ src/main/java/com/cliffc/aa/node/FP2DispNode.java
@@ -9,15 +9,33 @@ public final class FP2DispNode extends Node {
   @Override public Node ideal(GVNGCM gvn, int level) {
     // If at a FunPtrNode, it is only making a TFP out of a code pointer and a
     // display.  Become the display (dropping the code pointer).
-    if( in(0) instanceof FunPtrNode ) {
-      FunPtrNode fptr = (FunPtrNode)in(0);
+    Node disp = fptr2disp(in(0));
+    if( disp != null ) return disp;
+    // If all inputs to an Unresolved are FunPtrs with the same Display, become that display.
+    if( in(0) instanceof UnresolvedNode ) {
+      for( Node in : in(0)._defs ) {
+        Node disp2 = fptr2disp(in);
+        if( disp==null ) disp = disp2;
+        else if( disp!=disp2 )
+          return null;
+      }
+      return disp;
+    }
+    return null;
+  }
+
+  Node fptr2disp( Node in ) {
+    if( in instanceof FunPtrNode ) {
+      FunPtrNode fptr = (FunPtrNode)in;
       if( !fptr.is_forward_ref() )
         return fptr.display();
     }
     return null;
   }
+  
   @Override public Type value(GVNGCM.Mode opt_mode) {
     Type t = val(0);
     return t instanceof TypeFunPtr ? ((TypeFunPtr)t)._disp.simple_ptr() : t.oob(TypeMemPtr.DISP_SIMPLE);
   }
+  @Override public TypeMem live_use(GVNGCM.Mode opt_mode, Node def ) { return def instanceof ThretNode ? TypeMem.ANYMEM : _live; }
 }
